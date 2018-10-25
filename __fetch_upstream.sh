9e791bd61618d057a93aa06240b8ecf198c490b6
Kent XU <kent119951@gmail.com>
2018-10-25 15:14:31 +0800

    modified fetch

--- __fetch_upstream.sh
+++ __fetch_upstream.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git remote add upstream https://github.com/yutiansut/QUANTAXIS_Webkit.git
+# git remote add upstream https://github.com/yutiansut/QUANTAXIS_Webkit.git
 git fetch upstream
 git merge upstream/master
 git push
